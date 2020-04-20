4d2de425a1279ede1de101c07d8b281118089476
naddi <damiano@ippoagri.it>
2020-04-20 20:46:48 +0200

    correzione file

--- dowload_and_start_docker_container.sh
+++ dowload_and_start_docker_container.sh
@@ -1,10 +1,8 @@
 
 
 docker build -t webpp_container .
-docker pull sorl
-
+docker pull solr
 docker network create --driver bridge sorl_net
 docker run  -p 4567:4567 -d --network=solr_net --name=webapp webpp_container
-
 docker run -p 8983:8983 --network=solr_net --name=solr_server -t solr 
 docker exec -it solr_server solr create_core -c corso_informatica
