037ae94951546d327aea23d75d56aa622f7d6c01
naddi <damiano@ippoagri.it>
2020-04-20 20:30:53 +0200

    creata configurazione docker

---
+++ dowload_and_start_docker_container.sh
@@ -0,0 +1,10 @@
+
+
+docker build -t webpp_container .
+docker pull sorl
+
+docker network create --driver bridge sorl_net
+docker run  -p 4567:4567 -d --network=solr_net --name=webapp webpp_container
+
+docker run -p 8983:8983 --network=solr_net --name=solr_server -t solr 
+docker exec -it solr_server solr create_core -c corso_informatica
